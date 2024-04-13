1dca1d88ce05bb5a770d6b2f9e072128aaf49338
rendizi <rendizi_off@gmail.com>
2024-04-13 07:35:05 +0500

    Update: Added landing page

---
+++ src/Pages/Landing.js
@@ -0,0 +1,47 @@
+import {ReactTyped} from "react-typed";
+import { OrbitSpace } from 'orbit-space'
+import '../styles/landing.css';
+import { Navbar } from "../Components/Navbar";
+
+export const Landing = () => {
+    return (
+        <>
+         <Navbar/>
+                <main className="max-h-0">
+       
+        <div className="hero h-svh" style={{/*backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'*/}}>
+            <div className="hero-overlay"></div>
+            <div className="hero-content text-center text-neutral-content bg-slate-950 rounded-3xl p-12 bg-opacity-80">
+                <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">
+                    Explore Star Wars metaverse's{" "}
+                    <ReactTyped
+                    strings={["characters", "planets", "starships"]}
+                    typeSpeed={100}
+                    loop
+                    backSpeed={20}
+                    cursorChar="|"
+                    showCursor={true}
+                    />
+                </h1>           
+
+                <figure className="max-w-screen-md mx-auto text-center">
+                    <svg className="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+                    </svg>
+                    <blockquote>
+                        <p className="text-2xl italic font-medium text-gray-900 dark:text-white">"A long time ago in a galaxy far, far away...."</p>
+                    </blockquote>
+                </figure>
+<br/>
+                </div>
+            </div>
+        </div>
+         <OrbitSpace className="h-screen">
+         
+        </OrbitSpace>
+       
+     </main>
+     </>
+    )
+}

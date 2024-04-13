8af1b01ae4abc74dcaf8daca4b1da43b242c27da
Baglanov Alikhan <120701599+rendizi@users.noreply.github.com>
2024-04-13 09:59:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { NotFound } from './Pages/NotFound';
 import { Landing } from './Pages/Landing';
 import { Charecters } from './Pages/Charecters';
 import { SpaceCrafts } from './Pages/SpaceCrafts';
-import { Worlds } from './Pages/Worlds';
+import { Planets } from './Pages/Planets';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route index element={<Landing />} />
-          <Route path="/worlds" element={<Worlds />} />
+          <Route path="/planets" element={<Planets />} />
             <Route path="/characters" element={<Charecters/>} />
           <Route path="/starship" element={<SpaceCrafts/>} />
-          <Route path="/*" element={<NotFound />} />
       </Routes>
   </BrowserRouter>
   );

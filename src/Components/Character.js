cdf3f74091438a4fc321ee40af5a9475d32995b9
rendizi <rendizi_off@gmail.com>
2024-04-14 16:54:07 +0500

    Fixed: fixed bug in character page

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -73,73 +73,75 @@ export const Character = (props) => {
         }, 1000); 
     };
 
-    return (
-        <div className="collapse bg-slate-950">
-            {loading ? (
-                <div className="flex justify-center items-center">
-                    <span className="loading loading-dots loading-sm"></span>
-                </div>
-            ) : (
-                <>
-                    <input type="checkbox" /> 
-                    <div className="text-xl font-medium flex flex-col items-center justify-center pb-10 px-4">
-                        <h2 className="text-xl font-semibold mb-2">{person.name}</h2>
-                        <p className="text-center text-sm"><span className="font-semibold"><FontAwesomeIcon icon={faRuler} className="mr-2" />Height:</span> {person.height}</p>
-                        <p className="text-center text-sm"><span className="font-semibold"><FontAwesomeIcon icon={faBalanceScale} className="mr-2" />Mass:</span> {person.mass}</p>
-                        <p className="text-center text-sm"><span className="font-semibold"><FontAwesomeIcon icon={faVenusMars} className="mr-2" />Gender:</span> {person.gender}</p>
-                    </div>
 
-                    <dialog id={person.url} className="modal">
-                        {modalLoading ? (
-                            <div className="flex justify-center items-center">
-                                <span className="loading loading-dots loading-sm"></span>
-                            </div>
-                        ) : (
-                            <div key={person.url} className="p-4 rounded-lg shadow-md flex flex-col items-center">
-                                <p><span className="font-semibold"><FontAwesomeIcon icon={faPalette} className="mr-2" />Hair Color:</span> {person.hair_color}</p>
-                                <p><span className="font-semibold"><FontAwesomeIcon icon={faPalette} className="mr-2" />Skin Color:</span> {person.skin_color}</p>
-                                <p><span className="font-semibold"><FontAwesomeIcon icon={faEye} className="mr-2" />Eye Color:</span> {person.eye_color}</p>
-                                <p><span className="font-semibold"><FontAwesomeIcon icon={faBirthdayCake} className="mr-2" />Birth Year:</span> {person.birth_year}</p>
+   
 
-                                <div>
-                                    <div className="mt-4">
-                                        <span className="font-semibold"><FontAwesomeIcon icon={faFilm} className="mr-2" />Films:</span>
-                                        <ul className="list-disc pl-4">
-                                            {films.map((film, index) => (
-                                                <li key={index}>{film}</li>
-                                            ))}
-                                        </ul>
-                                    </div>
+          
 
-                                    <div className="mt-4">
-                                        <span className="font-semibold"><FontAwesomeIcon icon={faCar} className="mr-2" />Vehicles:</span>
-                                        <ul className="list-disc pl-4">
-                                            {vehicles.map((vehicle, index) => (
-                                                <li key={index}>{vehicle}</li>
-                                            ))}
-                                        </ul>
-                                    </div>
+        //<div>
+        
 
-                                    <div className="mt-4">
-                                        <span className="font-semibold"><FontAwesomeIcon icon={faSpaceShuttle} className="mr-2" />Starships:</span>
-                                        <ul className="list-disc pl-4">
-                                            {starships.map((starship, index) => (
-                                                <li key={index}>{starship}</li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        )}
-                        <form method="dialog" className="modal-backdrop">
-                            <button>close</button>
-                        </form>
-                    </dialog>
-                    <button className="btn" onClick={handleModalOpen}>View more</button>
-                </>
-            )}
+return (
+    <div className="collapse bg-slate-950">
+{loading ? (
+    <div className="flex justify-center items-center">
+        <span className="loading loading-dots loading-sm"></span>
+    </div>
+) : (
+    <div className='collapse'>
+        <input type="checkbox" /> 
+        <div className="text-xl font-medium flex flex-col items-center justify-center pb-10 px-4">
+        <h2 className="text-xl font-semibold mb-2">{person.name}</h2>
+        <p className="text-center text-sm"><span className="font-semibold"><FontAwesomeIcon icon={faRuler} className="mr-2" />Height:</span> {person.height}</p>
+        <p className="text-center text-sm"><span className="font-semibold"><FontAwesomeIcon icon={faBalanceScale} className="mr-2" />Mass:</span> {person.mass}</p>
+        <p className="text-center text-sm"><span className="font-semibold"><FontAwesomeIcon icon={faVenusMars} className="mr-2" />Gender:</span> {person.gender}</p>
         </div>
-    );
+
+        <dialog id={person.url} className="modal">
+            <div className="modal-box p-4 rounded-lg shadow-md flex flex-col items-center">
+            <p><span className="font-semibold"><FontAwesomeIcon icon={faPalette} className="mr-2" />Hair Color:</span> {person.hair_color}</p>
+        <p><span className="font-semibold"><FontAwesomeIcon icon={faPalette} className="mr-2" />Skin Color:</span> {person.skin_color}</p>
+        <p><span className="font-semibold"><FontAwesomeIcon icon={faEye} className="mr-2" />Eye Color:</span> {person.eye_color}</p>
+        <p><span className="font-semibold"><FontAwesomeIcon icon={faBirthdayCake} className="mr-2" />Birth Year:</span> {person.birth_year}</p>
+
+                <div className="mt-4">
+        <span className="font-semibold"><FontAwesomeIcon icon={faFilm} className="mr-2" />Films:</span>
+        <ul className="list-disc pl-4">
+            {films.map((film, index) => (
+                <li key={index}>{film}</li>
+            ))}
+        </ul>
+    </div>
+
+    <div className="mt-4">
+        <span className="font-semibold"><FontAwesomeIcon icon={faCar} className="mr-2" />Vehicles:</span>
+        <ul className="list-disc pl-4">
+            {vehicles.map((vehicle, index) => (
+                <li key={index}>{vehicle}</li>
+            ))}
+        </ul>
+    </div>
+
+    <div className="mt-4">
+        <span className="font-semibold"><FontAwesomeIcon icon={faSpaceShuttle} className="mr-2" />Starships:</span>
+        <ul className="list-disc pl-4">
+            {starships.map((starship, index) => (
+                <li key={index}>{starship}</li>
+            ))}
+        </ul>
+    </div>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
+    </div>
+)}
+<button className="btn self-end mb-0" onClick={() => {document.getElementById(person.url).showModal();}}>View more</button>
+</div>
+
+);
+
 };
 
 export default Character;
